e893f954ea40261c159aab18b5bb5c83d4de8659
chris <cseiter@gmail.com>
2021-09-16 13:34:23 -0400

    create RoomForm.js

---
+++ src/components/RoomForm.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+
+function RoomForm({onAddRoom}) {
+
+    const [formData, setFormData] = useState({
+        room: "",
+    });
+
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const newRoom = {...formData};
+
+        fetch('https://radiant-waters-01618.herokuapp.com/rooms', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newRoom),
+        })
+        .then((r) => r.json())
+        .then(onAddRoom);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h3>Add New Room</h3>
+                <input 
+                type="text"
+                name="room"
+                onChange={handleChange}
+                value={formData.room}
+                placeholder="Enter room name"
+                />
+                <br />
+                <input 
+                type="submit"
+                name="submit"
+                value="Add Room"
+                />
+            </form>
+        </div>
+    );
+};
+
+export default RoomForm;
