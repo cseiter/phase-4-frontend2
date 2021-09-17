194d851ad0876ab58cc21cea1d91cb36b3592185
chris <cseiter@gmail.com>
2021-09-16 20:11:00 -0400

    Delete commented out section in App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import './App.css';
-//import React, {useEffect, useState} from 'react';
-//import RoomContainer from './components/RoomContainer';
 import RoomPage from './components/Rooms/RoomPage';
 
 function App() {
-  // const [showForm, setShowForm] = useState(false);
-//  const [rooms,setRooms] = useState([]);
-
-//  useEffect(() => {
-//    fetch('https://radiant-waters-01618.herokuapp.com/rooms')
-//    .then((r) => r.json())
-//    .then(setRooms);
-//  },[]);
-
-//  function handleClick() {
-//    setShowForm((showForm) => !showForm);
-//  }
-
-//  function handleDeleteRoom(roomToDelete) {
-//    const updatedRooms = rooms.filter((room) => room.id !== roomToDelete.id);
-//    setRooms(updatedRooms);
-//  }
 
   return (
     <div className="App">
-{/*       <main>
-        <RoomContainer 
-        rooms={rooms}
-        onDeleteRoom={handleDeleteRoom}
-        />
-      </main> */}
       <RoomPage />
     </div>
   );
