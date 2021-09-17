27b50152ab3a94a40b3867e45a591cac43418ead
chris <cseiter@gmail.com>
2021-09-16 20:47:18 -0400

    update App.js with CategoryPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import RoomPage from './components/Rooms/RoomPage';
+import CategoryPage from './components/Categories/CategoryPage';
 
 function App() {
 
   return (
     <div className="App">
       <RoomPage />
+      <CategoryPage />
     </div>
   );
 }
