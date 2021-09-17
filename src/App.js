71743d8199993d2931a1de13c8c6214cf6ce51dc
chris <cseiter@gmail.com>
2021-09-16 21:12:31 -0400

    added routes and switches to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
 import RoomPage from './components/Rooms/RoomPage';
 import CategoryPage from './components/Categories/CategoryPage';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className="App">
+    <center>
+    <Router>
+      <div>
+        {/* <Link to="/">Home</Link> */}
+        <Link to="/rooms">Rooms</Link>
+        <Link to="/categories">Categories</Link>
+
+        <Switch>
+{/*           <Route exact path="/">
+            <Home />
+          </Route> */}
+          <Route exact path="/rooms">
+            <RoomPage />
+          </Route>
+          <Route exact path="/categories">
+            <CategoryPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    </center>
+
+ /*    <div className="App">
       <RoomPage />
       <CategoryPage />
-    </div>
+    </div> */
   );
 }
 
