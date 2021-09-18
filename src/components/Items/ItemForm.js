64ecf0e642356f1887dcd47e80a64c469a3e5bf0
chris <cseiter@gmail.com>
2021-09-18 11:07:19 -0400

    remove general bootstrap import to keep with page style wanted

--- src/components/Items/ItemForm.js
+++ src/components/Items/ItemForm.js
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 
