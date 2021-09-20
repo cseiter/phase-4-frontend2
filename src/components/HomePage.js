808507c261140d180901ef2567c609cd3374e466
chris <cseiter@gmail.com>
2021-09-19 20:01:15 -0400

    fix typo and other misses in homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 //import { Link } from 'react-router-dom';
-import '../../App.css';
+import '../App.css';
 
 function HomePage() {
 
+    return(
     <div>
         Get your S. H. I. T. together!
     </div>
+    )
 };
 
 export default HomePage;
